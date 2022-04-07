02362d80ee5dffbf896f24a6a414d3d413067a6b
Krishna sai Patel <herorocks56@gmail.com>
2022-04-07 15:47:17 -0700

    1st algo done

--- Absolute Values Sum Minimization/absoluteValuesSumMinization.ts
+++ Absolute Values Sum Minimization/absoluteValuesSumMinization.ts
@@ -1,2 +1,12 @@
-console.log(2 + 3);
+const absoluteValuesSumMinization = (arr: number[]): number => { 
+    const isEven = arr.length % 2 === 0
+    const mid = arr.length / 2
+    if (isEven) {
+        return arr[mid - 1]
+    } else {
+        const elementIndex = Math.floor(mid)
+        return arr[elementIndex]
+    } 
+}
 
+console.log(absoluteValuesSumMinization([2, 4, 7, 6, 6, 8]))  
